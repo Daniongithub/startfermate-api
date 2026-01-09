848ba247e656ee8816e11bb75b7ae2cb5486d06a
Curry141 <135717077+Daniongithub@users.noreply.github.com>
2026-01-09 19:30:27 +0100

    Small filter to remove undesired text.

--- server.js
+++ server.js
@@ -5,188 +5,193 @@ const cors = require('cors');
 
 const app = express();
 const port = 3005;
-const version = "2.2.3";
+const version = "2.3.0";
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/fermata', async (req, res) => {
-    try {
-        const { param, param2, palina } = req.query;
-        const url = `https://infobus.startromagna.it/InfoFermata?param=${param}&param2=${param2}&palina=${palina}`;
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-        const results = [];
-        var fermata = $('h2.fw-bold.text-primary.title').text().trim();
-        if(fermata != ""){
-          results.push({
-          fermata
-          });
-        }
-        $('.container.mb-50 .bus-card').each((i, el) => {
-            const element = $(el);
-            const isSoppressa = element.find('.bus-status.sopp').length > 0;
-            const headerSpan = element.find('.bus-header span').first();
-            const orario = element.find('.bus-times span').first().text().trim();
-            var stato = element.find('.bus-status').text().trim();
-            var linea = headerSpan.contents().filter((i, el) => el.type === 'text').text().trim();
-            linea = linea.replace("Linea ", "");
-            var destinazione = element.find('.bus-destination').text().trim();
-            var mezzo = element.find('.det a').attr('data-vehicle') || '';
-
-            //Aggiustamenti
-
-            if(mezzo.length == 4){
-              mezzo = 3 + mezzo;
-            }
-
-            if(stato == "Non disp"){
-              stato = "N.D.";
-            }
-
-            if(mezzo == ""){
-              mezzo = "N.D.";
-            }
-
-            if(destinazione == "Fornace.Zarattini"){
-              destinazione = "Fornace Zarattini"
-            }
-
-            if(linea == "4" && destinazione == "Classe"){
-              destinazione = "Classe Piazza";
-              linea = "4R";
-            }
-
-            //Varianti linee
-
-            if(linea == "1" && destinazione == "Borgo Nuovo"){
-              linea = "1B";
-            }
-
-            if(linea == "4" && destinazione == "Mirabilandia"){
-              linea = "4B";
-            }
-
-            if(linea == "4" && destinazione == "Classe Cantoniera"){
-              linea = "4C";
-            }
-
-            if(linea == "4" && destinazione == "ClasseCantoniera"){
-              linea = "4C";
-            }
-
-            if(linea == "4" && destinazione == "Lido di Dante"){
-              linea = "4D";
-            }
-
-            if(linea == "4" && destinazione == "Fosso Ghiaia"){
-              linea = "4F";
-            }
-
-            if(linea == "4" && destinazione == "Fosso Ghiaia-Standiana"){
-              linea = "4F";
-            }
-
-            if(linea == "4" && destinazione == "Savio di Cervia"){
-              linea = "4F";
-            }
-
-            if(linea == "4" && destinazione == "Classe via Liburna"){
-              linea = "4R";
-            }
-
-            if(linea == "4" && destinazione == "Classe Romea Vecchia"){
-              linea = "4R";
-            }
-
-            if(linea == "4" && destinazione == "R.Vecchia"){
-              linea = "4R";
-            }
-
-            if(linea == "4" && destinazione == "Romea Vecchia via del Centurione"){
-              linea = "4C";
-            }
-
-            //Linee limitate o soppresse a metà
-
-            if(linea == "8" && destinazione == "Deposito"){
-              linea = "8/";
-            }
-
-            if(linea == "3" && destinazione == "via Sant'Alberto"){
-              linea = "3/";
-            }
-
-            if(linea == "1" && destinazione == "Antica Milizia"){
-              linea = "1/";
-            }
-
-            if(linea == "1" && destinazione == "Via Antica Milizia"){
-              linea = "1/";
-            }
-
-            const linee = ["1", "1B", "3", "4", "4B", "4C", "4D", "4F", "4R", "5", "8", "18", "70", "80"];
-
-            if(linee.includes(linea) && destinazione == "Stazione FS"){
-              linea = linea + "/";
-            }
-
-            if(linee.includes(linea) && destinazione == "STAZIONE FS"){
-              linea = linea + "/";
-            }
-
-            if(linee.includes(linea) && destinazione == "Ravenna FS"){
-              linea = linea + "/";
-            }
-
-            if(linee.includes(linea) && destinazione == "Ravenna Radio Taxi"){
-              linea = linea + "/";
-            }
-
-            results.push({
-                linea,
-                destinazione,
-                orario,
-                stato,
-                mezzo,
-                soppressa: isSoppressa
-            });
-        });
-
-        res.json(results);
-    } catch (error) {
-        console.error('Errore:', error.message);
-        res.status(500).send('Errore nel recupero dei dati');
+  try {
+    const { param, param2, palina, det } = req.query;
+    const url = `https://infobus.startromagna.it/InfoFermata?param=${param}&param2=${param2}&palina=${palina}`;
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const results = [];
+    let fermata = $('h2.fw-bold.text-primary.title').text().trim();
+    if (fermata != "") {
+      results.push({
+        fermata
+      });
     }
+    $('.container.mb-50 .bus-card').each((i, el) => {
+      const element = $(el);
+      const isSoppressa = element.find('.bus-status.sopp').length > 0;
+      const headerSpan = element.find('.bus-header span').first();
+      const orario = element.find('.bus-times span').first().text().trim();
+      let stato = element.find('.bus-status').text().trim();
+      let linea = headerSpan.contents().filter((i, el) => el.type === 'text').text().trim();
+      linea = linea.replace("Linea ", "");
+      let destinazione = element.find('.bus-destination').text().trim();
+      let mezzo = element.find('.det a').attr('data-vehicle') || '';
+
+      //Aggiustamenti
+
+      if (mezzo.length == 4) {
+        mezzo = 3 + mezzo;
+      }
+
+      if (stato == "Non disp") {
+        stato = "N.D.";
+      }
+
+      if (mezzo == "") {
+        mezzo = "N.D.";
+      }
+
+      if (destinazione == "Fornace.Zarattini") {
+        destinazione = "Fornace Zarattini"
+      }
+
+      if (linea == "4" && destinazione == "Classe") {
+        destinazione = "Classe Piazza";
+        linea = "4R";
+      }
+
+      //letianti linee
+
+      if (linea == "1" && destinazione == "Borgo Nuovo") {
+        linea = "1B";
+      }
+
+      if (linea == "4" && destinazione == "Mirabilandia") {
+        linea = "4B";
+      }
+
+      if (linea == "4" && destinazione == "Classe Cantoniera") {
+        linea = "4C";
+      }
+
+      if (linea == "4" && destinazione == "ClasseCantoniera") {
+        linea = "4C";
+      }
+
+      if (linea == "4" && destinazione == "Lido di Dante") {
+        linea = "4D";
+      }
+
+      if (linea == "4" && destinazione == "Fosso Ghiaia") {
+        linea = "4F";
+      }
+
+      if (linea == "4" && destinazione == "Fosso Ghiaia-Standiana") {
+        linea = "4F";
+      }
+
+      if (linea == "4" && destinazione == "Savio di Cervia") {
+        linea = "4F";
+      }
+
+      if (linea == "4" && destinazione == "Classe via Liburna") {
+        linea = "4R";
+      }
+
+      if (linea == "4" && destinazione == "Classe Romea Vecchia") {
+        linea = "4R";
+      }
+
+      if (linea == "4" && destinazione == "R.Vecchia") {
+        linea = "4R";
+      }
+
+      if (linea == "4" && destinazione == "Romea Vecchia via del Centurione") {
+        linea = "4C";
+      }
+
+      //Linee limitate o soppresse a metà
+
+      if (linea == "8" && destinazione == "Deposito") {
+        linea = "8/";
+      }
+
+      if (linea == "3" && destinazione == "via Sant'Alberto") {
+        linea = "3/";
+      }
+
+      if (linea == "1" && destinazione == "Antica Milizia") {
+        linea = "1/";
+      }
+
+      if (linea == "1" && destinazione == "Via Antica Milizia") {
+        linea = "1/";
+      }
+
+      const linee = ["1", "1B", "3", "4", "4B", "4C", "4D", "4F", "4R", "5", "8", "18", "70", "80"];
+
+      if (linee.includes(linea) && destinazione == "Stazione FS") {
+        linea = linea + "/";
+      }
+
+      if (linee.includes(linea) && destinazione == "STAZIONE FS") {
+        linea = linea + "/";
+      }
+
+      if (linee.includes(linea) && destinazione == "Ravenna FS") {
+        linea = linea + "/";
+      }
+
+      if (linee.includes(linea) && destinazione == "Ravenna Radio Taxi") {
+        linea = linea + "/";
+      }
+
+      //Pulisco la spazzatura che mandano le AVM da circa natale 2025
+      const azioni = ["RIENTRO DEPOSITO", "PRESA SERVIZIO", "FUORI LINEA"];
+
+      if(det == "true" || !azioni.includes(destinazione)){
+        results.push({
+          linea,
+          destinazione,
+          orario,
+          stato,
+          mezzo,
+          soppressa: isSoppressa
+        });
+      }
+    });
+
+    res.json(results);
+  } catch (error) {
+    console.error('Errore:', error.message);
+    res.status(500).send('Errore nel recupero dei dati');
+  }
 });
 
 app.get('/bacino', async (req, res) => {
-    const selectedOption = req.query.selectedOption;
+  const selectedOption = req.query.selectedOption;
 
-    if (!selectedOption || !['ra', 'rn', 'fc'].includes(selectedOption)) {
-        return res.status(400).json({ error: 'Parametro selectedOption mancante o non valido' });
+  if (!selectedOption || !['ra', 'rn', 'fc'].includes(selectedOption)) {
+    return res.status(400).json({ error: 'Parametro selectedOption mancante o non valido' });
+  }
+
+  try {
+    const response = await axios.post('https://infobus.startromagna.it/FermateService.asmx/GetFermateByBacino', {
+      bacino: selectedOption
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const raw = response.data.d;
+    const data = raw.map(({ nome, palina, targetID }) => ({ nome, palina, targetID }));
+    if (Array.isArray(data)) {
+      res.json(data);
+    } else {
+      res.status(500).json({ error: 'Formato di risposta non valido dal server remoto' });
     }
-
-    try {
-        const response = await axios.post('https://infobus.startromagna.it/FermateService.asmx/GetFermateByBacino', {
-            bacino: selectedOption
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const raw = response.data.d;
-        const data = raw.map(({ nome, palina, targetID }) => ({ nome, palina, targetID }));
-        if (Array.isArray(data)) {
-          res.json(data);
-          } else {
-          res.status(500).json({ error: 'Formato di risposta non valido dal server remoto' });
-          }
-    } catch (error) {
-        console.error('Errore nella richiesta al servizio remoto:', error);
-        res.status(500).json({ error: 'Errore nel contattare il servizio remoto' });
-    }
+  } catch (error) {
+    console.error('Errore nella richiesta al servizio remoto:', error);
+    res.status(500).json({ error: 'Errore nel contattare il servizio remoto' });
+  }
 });
 
 app.get('/versione', async (req, res) => {
@@ -194,5 +199,5 @@ app.get('/versione', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`API attiva su http://localhost:${port}`);
+  console.log(`API attiva su http://localhost:${port}`);
 });
